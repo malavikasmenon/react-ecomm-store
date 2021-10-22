231c611b8fb31e094a99e1f69f22ce1c01b21e78
malavikasmenon <malavikamenon2000@gmail.com>
2021-10-23 01:06:53 +0530

    product filters

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,9 +6,12 @@ function Nav() {
   let cart_count = useSelector((state) => state.cartCount.cart_count);
   return ( 
     <nav className="nav-wrapper">
-      <Link to='/' className="nav-item">Home</Link>
-      <Link to='/products' className="nav-item">Products</Link>
-      <Link to='/cart' className="nav-item">Cart ({cart_count})</Link>
+      <h1>WindowStore</h1>
+      <div className="nav-link">
+        <Link to='/' className="nav-item">Home</Link>
+        <Link to='/products' className="nav-item">Products</Link>
+        <Link to='/cart' className="nav-item">Cart ({cart_count})</Link>
+      </div>
     </nav>
   );
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -8,6 +8,8 @@ function Products() {
   const [error, setError] = useState(null);
   const [isLoaded, setLoaded] = useState(false);
   const [products, setProducts] = useState([]);
+  const [categories, setCategory] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
   let cart_count = useSelector((state) => state.cartCount.cart_count);
   console.log("yo", cart_count);
   const dispatch = useDispatch();
@@ -18,14 +20,28 @@ function Products() {
     })
       .then(res=>res.json())
       .then((result)=> {
-        console.log(result)
+        console.log('one')
         setLoaded(true)
         setProducts(result)
+        setFilteredList(result)
       },
       (error) => {
         setLoaded(true);
         setError(error);
       })
+      fetch('https://fakestoreapi.com/products/categories', {
+      'Access-Control-Allow-Origin': '*',
+    })
+      .then(res=>res.json())
+      .then((result)=> {
+        console.log('wgere',result)
+        setCategory(result);
+      },
+      (error) => {
+        console.log(error);
+        setError(true);
+      })
+    
   }, [])
 
   function addCart(id){
@@ -44,6 +60,30 @@ function Products() {
     dispatch(setCartTotal(total));
   }
 
+  function changeCategory(e){
+    console.log(e.target.value);
+    if(e.target.value!=='all'){
+      if(filteredList.length > 0)
+        setFilteredList(filteredList.filter(el => el.category===e.target.value))
+      else
+        setFilteredList(products.filter(el => el.category===e.target.value))
+    }
+    else
+      setFilteredList(products);
+  }
+
+  function changePriceRange(e){
+    if(e.target.value!=="none"){
+      if(filteredList.length > 0)
+        setFilteredList(filteredList.filter(el => el.price < e.target.value))
+      else
+      setFilteredList(products.filter(el => el.price < e.target.value))
+    }
+    else
+      setFilteredList(products);
+      
+  }
+
   if(error) {
     return <div>Error: {error.message}</div>
   }
@@ -54,8 +94,25 @@ function Products() {
     return ( 
       <div>
         <h1>Products</h1>
+        <label>Category</label>
+        <select onChange={changeCategory} className="input-dropdown">
+          <option key="all" value="all" selected>All</option>
+          {
+            categories.map(el => (
+            <option key={el} value={el}>{el}</option>
+            ))
+          }    
+        </select>
+        <label>Price Range (below)</label>
+        <select onChange={changePriceRange} className="input-dropdown">
+          <option value="none">No filter</option>
+          <option value="10">$10</option>
+          <option value="50">$50</option>
+          <option value="100">$100</option>
+          <option value="200">$200</option>
+        </select>
         <ul className="prod-container">
-          {products.map(el => (
+          {filteredList.map(el => (
             <li key={el.id} className="product">
               <Link to={`product/${el.id}`}>
                 <img src={el.image} className="prod-image" alt="the product"></img>
